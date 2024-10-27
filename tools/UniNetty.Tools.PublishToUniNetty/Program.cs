4b9d5ed82609e81b28a030a56ee8d5fc7b6c4d22
ikpil <ikpil@naver.com>
2024-10-27 22:50:37 +0900

    added publish to uninetty

---
+++ tools/UniNetty.Tools.PublishToUniNetty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniNetty.Tools.PublishToUniNetty;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        return 0;
+    }
+}
