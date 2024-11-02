9339c697221388392b5dccaa511785243a436b84
ikpil <ikpil@naver.com>
2024-11-03 01:21:48 +0900

    test

--- tools/UniNetty.Tools.FileChunk/Program.cs
+++ tools/UniNetty.Tools.FileChunk/Program.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 class Program
 {
     static int Main(string[] args)
     {
-        string driveLetter = $"{args[1]}:\\";
+        string driveLetter = $"{args[0]}:\\";
         long totalSize = GetFreeSpace(driveLetter);
         int blockSize = 1 * 1024 * 1024 * 100;
         int numBlocks = (int)(totalSize / blockSize);
 
         Console.WriteLine($"free size : {totalSize / (1024 * 1024)}MB");
-        
+
         string input = Console.ReadLine();
         if (input?.Trim().ToLower() != "start")
             return -1;
+
+        byte[] buffer = new byte[blockSize];
+        Random random = new Random();
+        random.NextBytes(buffer);
 
         var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 70 };
         Parallel.For(0, numBlocks, parallelOptions, i =>
         {
             string tempFilePath = Path.Combine(driveLetter, $"temp_{Path.GetRandomFileName()}.bin");
-            
-            byte[] buffer = new byte[blockSize];
-            Random random = new Random();
-            random.NextBytes(buffer);
 
             using var fs = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write, FileShare.None);
             fs.Write(buffer, 0, buffer.Length);
