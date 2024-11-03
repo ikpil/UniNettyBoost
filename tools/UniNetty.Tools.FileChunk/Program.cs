ef1363a3389584724c9e6597ee3a9c654f639530
ikpil <ikpil@naver.com>
2024-11-04 02:23:32 +0900

    test 2

--- tools/UniNetty.Tools.FileChunk/Program.cs
+++ tools/UniNetty.Tools.FileChunk/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 class Program
@@ -13,25 +16,35 @@ class Program
         int numBlocks = (int)(totalSize / blockSize);
 
         Console.WriteLine($"free size : {totalSize / (1024 * 1024)}MB");
+        Console.WriteLine("If you really want to start, type 'start'.");
 
         string input = Console.ReadLine();
         if (input?.Trim().ToLower() != "start")
             return -1;
 
         byte[] buffer = new byte[blockSize];
-        Random random = new Random();
-        random.NextBytes(buffer);
 
-        var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 70 };
-        Parallel.For(0, numBlocks, parallelOptions, i =>
+        var cancelBag = new ConcurrentBag<long>();
+        RegisterCancel(cancelBag);
+
+
+        for (int i = 0; i < numBlocks; ++i)
         {
+            if (!cancelBag.IsEmpty)
+                break;
+
+            Random random = new Random();
+            random.NextBytes(buffer);
+
             string tempFilePath = Path.Combine(driveLetter, $"temp_{Path.GetRandomFileName()}.bin");
+
+            Console.WriteLine($"creating file : {tempFilePath}");
 
             using var fs = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write, FileShare.None);
             fs.Write(buffer, 0, buffer.Length);
 
-            Console.WriteLine($"create file : {tempFilePath}");
-        });
+            Console.WriteLine($"created file : {tempFilePath}");
+        }
 
         Console.WriteLine("Done");
 
@@ -42,5 +55,24 @@ class Program
     {
         DriveInfo driveInfo = new DriveInfo(driveLetter);
         return driveInfo.AvailableFreeSpace;
+    }
+
+    static void RegisterCancel(ConcurrentBag<long> bag)
+    {
+        ConsoleCancelEventHandler handler = (sender, e) =>
+        {
+            if (ConsoleSpecialKey.ControlC == e.SpecialKey || ConsoleSpecialKey.ControlBreak == e.SpecialKey)
+            {
+                e.Cancel = true;
+                bag.Add(DateTime.UtcNow.Ticks);
+            }
+            else
+            {
+                // 코드 스트림이 여기까지 도달 할 수 있는가?
+                e.Cancel = false;
+            }
+        };
+
+        Console.CancelKeyPress += handler;
     }
 }
